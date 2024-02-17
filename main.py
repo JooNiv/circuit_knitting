2cbb96e2af212d9a0c8d56f3245ce92773636af0
Joonas Nivala <joonas.nivala@gmail.com>
2024-02-17 20:58:52 +0200

    cleaned up code a bit

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from qiskit.quantum_info import PauliList
 
 #original full circuit
 qc = QuantumCircuit(3)
-qc.x(0)
+qc.h(0)
 qc.cx(0,1)
 qc.cx(1,2)
 qc.cx(0,1)
@@ -18,7 +18,7 @@ print(qc)
 
 #circuit ready for partitioning with cutWire instructions inserted
 qca = QuantumCircuit(4)
-qca.x(0)
+qca.h(0)
 qca.cx(0,1)
 qca.append(cutWire, [1,2])
 qca.cx(2,3)
@@ -31,11 +31,8 @@ print(qca)
 #get locations of the cuts
 qss = ck.get_cut_location(qca)
 
-#print(qss)
-
 #insert placeholders for the masure and initialize nodes
 circ = ck.insert_placeholder(qca, qss)
-circ = circ.decompose(gates_to_decompose="decomp")
 
 print("Circuit ready to be cut-------------------------------------------------------------------------------")
 print(circ)
@@ -53,17 +50,14 @@ ds = ck.decomp_combinations(qss)
 #make the experiment circuits
 allcircs, coefs = ck.all_subcircs(ds, circs, qss)
 
-#run the experiments
+#run the experiments, apply the {0,1} -> [-1,1] post-processing function
 results = ck.run_experiments(allcircs)
 
-#apply the {0,1} -> [-1,1] post-processing function
-modres = ck.modify_results(results)
-
 #zip coefficients and results together
-data = list(zip(coefs, modres))
+data = list(zip(coefs, results))
 
 #sample the experiment results and calculate the expectation value
-f = ck.sample_results(data, qss)
+f = ck.sample_results(data)
 print("Estimated expectation values--------------------------------------------------------------------------")
 print(f)
 
